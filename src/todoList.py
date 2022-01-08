5612d8f129bb60a858bd13e83f87391a575e72f9
Ubuntu <ubuntu@ip-172-31-11-167.ec2.internal>
2022-01-08 11:20:36 +0000

    commented if URL in get_table 4

--- src/todoList.py
+++ src/todoList.py
@@ -3,7 +3,7 @@ import boto3
 import time
 import uuid
 import json
-import functools
+# import functools
 from botocore.exceptions import ClientError
 
 
@@ -17,7 +17,7 @@ from botocore.exceptions import ClientError
 
 def get_table(dynamodb=None):
     if not dynamodb:
-        URL = os.environ['ENDPOINT_OVERRIDE']
+        # URL = os.environ['ENDPOINT_OVERRIDE']
         # if URL:
         #    print('URL dynamoDB:'+URL)
         #    boto3.client = functools.partial(boto3.client, endpoint_url=URL)
