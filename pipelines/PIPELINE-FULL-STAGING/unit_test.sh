e0b17f56d161ee44335c70bd6cd96c5bbc1b61bc
Ubuntu <ubuntu@ip-172-31-11-167.ec2.internal>
2022-01-10 14:04:20 +0000

    recover original test-unit and PIPE-STAGING  folders to avoid internal server error once deployed

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -4,15 +4,9 @@ source todo-list-aws/bin/activate
 set -x
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
-#PGS: set variable with table name to use it in todoList.py
 export DYNAMODB_TABLE=todoUnitTestsTable
-export ENDPOINT_OVERRIDE='http://localhost:8000'
-#PGS: Execute TestToDo.py
 python test/unit/TestToDo.py
 pip show coverage
-#PGS: Run a Python program and collect execution data. We must get over 80%
 coverage run --include=src/todoList.py test/unit/TestToDo.py
-#PGS: Report coverage results. 
-coverage report -m
-#PGS: Produce an XML report with coverage results.
+coverage report
 coverage xml
