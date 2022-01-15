c4bce330a91ca564a6ba654893b5ad046589da4a
Ubuntu <ubuntu@ip-172-31-11-167.ec2.internal>
2022-01-15 08:26:10 +0000

    start dynamo docker if exited  and create table

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -7,6 +7,14 @@ echo "PYTHONPATH: $PYTHONPATH"
 #PGS: set variable with table name to use it in todoList.py
 export DYNAMODB_TABLE=todoUnitTestsTable
 export ENDPOINT_OVERRIDE='http://localhost:8000'
+
+# PGS: start dynamo docker if is exited, for coverage test
+if [ ! "$(docker ps -q -f name=dynamodb)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=dynamodb)" ]; then
+        docker start -i dynamodb
+        aws dynamodb create-table --table-name local-TodosDynamoDbTable --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://localhost:8000 --region us-east-1
+    fi
+fi
 #PGS: Execute TestToDo.py
 python test/unit/TestToDo.py
 pip show coverage
@@ -16,3 +24,4 @@ coverage run --include=src/todoList.py test/unit/TestToDo.py
 coverage report -m
 #PGS: Produce an XML report with coverage results.
 coverage xml
+
